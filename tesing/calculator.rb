07f86e283e83e8e6f690c7c4560548da68c9bfc8
jaichandru-interloid <jaichandru@interloid.com>
2026-08-07 19:20:41 +0530

    Today learned modules

--- tesing/calculator.rb
+++ tesing/calculator.rb
@@ -3,3 +3,10 @@ class Calculator
     a + b
   end
 end
+
+# class Calculator
+#   def divide (a, b)
+#     raise ZeroDivisionError, "Can't divide by Zero" if b == 0
+#     a / b
+#   end
+# end
--- tesing/test_calculator.rb
+++ tesing/test_calculator.rb
@@ -1,12 +1,54 @@
 # frozen_string_literal: true
 
-require 'rspec'
-require './calculator'
+# # frozen_string_literal: true
+
+# require 'rspec'
+# require './calculator'
+
+# RSpec.describe Calculator do
+#   it 'add two numbers' do
+#     calc = Calculator.new
+
+#     expect(calc.add(10, 12)).to eq(22)
+#   end
+# end
+
+# require_relative 'calculator'
+
+# RSpec.describe Calculator do
+#   let(:calculator) { Calculator.new }
+
+#   describe '#divide' do
+#     context 'when divisor is not zero' do
+#       it 'returns the quotient' do
+#         expect(calculator.divide(10, 2)).to eq(5)
+#       end
+#     end
+
+#     context 'when divisor is zero' do
+#       it 'raises ZeroDivisionError' do
+#         expect { calculator.divide(10, 0) }.to raise_error(ZeroDivisionError)
+#       end
+#     end
+#   end
+# end
+
+#around example program
+require_relative "calculator"
 
 RSpec.describe Calculator do
-  it 'add two numbers' do
-    calc = Calculator.new
+  let(:calculator) { Calculator.new }
 
-    expect(calc.add(10, 12)).to eq(22)
+  around do |example|
+    puts "Starting Test"
+
+    example.run
+
+    puts "Ending Test"
+  end
+
+  it "adds numbers" do
+    puts "Testing...!"
+    expect(calculator.add(10, 20)).to eq(30)
   end
 end
